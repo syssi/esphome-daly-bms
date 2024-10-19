d7a3edda4a8dedfb6bf63ff174bbf86ed29e34f5
Sebastian Muszynski <basti@linkt.de>
2024-10-19 22:44:49 +0200

    Add comment

--- components/daly_bms_ble/button/daly_button.cpp
+++ components/daly_bms_ble/button/daly_button.cpp
@@ -20,7 +20,8 @@ void DalyButton::press_action() {
     return;
   }
 
-  this->parent_->send_command(DALY_FUNCTION_WRITE, this->holding_register_, 0x0000);
+  this->parent_->send_command(DALY_FUNCTION_WRITE, this->holding_register_,
+                              0x0000);  // @FIXME: This payload is just a guess
 }
 
 }  // namespace daly_bms_ble
