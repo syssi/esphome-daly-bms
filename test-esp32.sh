ab9550ed6f6f85e90f96f0c05c099b1c31cd0c63
Sebastian Muszynski <basti@linkt.de>
2024-10-21 10:25:09 +0200

    Improve README (#16)

--- test-esp32.sh
+++ test-esp32.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-esphome -s external_components_source components ${1:-run} ${2:-esp32-ble-example-faker.yaml}
+esphome -s mac_address B0:A7:32:13:57:72 -s external_components_source components ${1:-run} ${2:-esp32-ble-example-faker.yaml}
