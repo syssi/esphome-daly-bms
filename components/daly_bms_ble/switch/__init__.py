5fdf85c6e98f46ef899474b1e64461325a07161f
Sebastian Muszynski <basti@linkt.de>
2025-05-25 17:54:22 +0200

    Fix `*.*_SCHEMA` deprecations (#44)

--- components/daly_bms_ble/switch/__init__.py
+++ components/daly_bms_ble/switch/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import switch
 import esphome.config_validation as cv
-from esphome.const import CONF_ICON, CONF_ID
+from esphome.const import CONF_ID
 
 from .. import CONF_DALY_BMS_BLE_ID, DALY_BMS_BLE_COMPONENT_SCHEMA, daly_bms_ble_ns
 
@@ -27,23 +27,14 @@ DalySwitch = daly_bms_ble_ns.class_("DalySwitch", switch.Switch, cg.Component)
 
 CONFIG_SCHEMA = DALY_BMS_BLE_COMPONENT_SCHEMA.extend(
     {
-        cv.Optional(CONF_BALANCER): switch.SWITCH_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(DalySwitch),
-                cv.Optional(CONF_ICON, default=ICON_BALANCER): cv.icon,
-            }
+        cv.Optional(CONF_BALANCER): switch.switch_schema(
+            DalySwitch, icon=ICON_BALANCER
         ).extend(cv.COMPONENT_SCHEMA),
-        cv.Optional(CONF_CHARGING): switch.SWITCH_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(DalySwitch),
-                cv.Optional(CONF_ICON, default=ICON_CHARGING): cv.icon,
-            }
+        cv.Optional(CONF_CHARGING): switch.switch_schema(
+            DalySwitch, icon=ICON_CHARGING
         ).extend(cv.COMPONENT_SCHEMA),
-        cv.Optional(CONF_DISCHARGING): switch.SWITCH_SCHEMA.extend(
-            {
-                cv.GenerateID(): cv.declare_id(DalySwitch),
-                cv.Optional(CONF_ICON, default=ICON_DISCHARGING): cv.icon,
-            }
+        cv.Optional(CONF_DISCHARGING): switch.switch_schema(
+            DalySwitch, icon=ICON_DISCHARGING
         ).extend(cv.COMPONENT_SCHEMA),
     }
 )
