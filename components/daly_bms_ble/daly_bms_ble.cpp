4d167fec8466f3d806774cfd527064beb8ba10a3
Sebastian Muszynski <basti@linkt.de>
2025-11-30 09:32:45 +0100

    Add ESPHome 2025.12 compatibility layer for address_str() API change

--- components/daly_bms_ble/daly_bms_ble.cpp
+++ components/daly_bms_ble/daly_bms_ble.cpp
@@ -1,6 +1,13 @@
 #include "daly_bms_ble.h"
 #include "esphome/core/log.h"
 #include "esphome/core/helpers.h"
+#include "esphome/core/version.h"
+
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2025, 12, 0)
+#define ADDR_STR(x) x
+#else
+#define ADDR_STR(x) (x).c_str()
+#endif
 
 namespace esphome {
 namespace daly_bms_ble {
@@ -137,7 +144,7 @@ void DalyBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t g
     case ESP_GATTC_SEARCH_CMPL_EVT: {
       auto *char_notify = this->parent_->get_characteristic(DALY_BMS_SERVICE_UUID, DALY_BMS_NOTIFY_CHARACTERISTIC_UUID);
       if (char_notify == nullptr) {
-        ESP_LOGE(TAG, "[%s] No notify service found at device, not an Daly BMS..?", this->parent_->address_str());
+        ESP_LOGE(TAG, "[%s] No notify service found at device, not an Daly BMS..?", ADDR_STR(this->parent_->address_str()));
         break;
       }
       this->char_notify_handle_ = char_notify->handle;
@@ -151,7 +158,7 @@ void DalyBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t g
       auto *char_command =
           this->parent_->get_characteristic(DALY_BMS_SERVICE_UUID, DALY_BMS_CONTROL_CHARACTERISTIC_UUID);
       if (char_command == nullptr) {
-        ESP_LOGE(TAG, "[%s] No control service found at device, not an Daly BMS..?", this->parent_->address_str());
+        ESP_LOGE(TAG, "[%s] No control service found at device, not an Daly BMS..?", ADDR_STR(this->parent_->address_str()));
         break;
       }
       this->char_command_handle_ = char_command->handle;
@@ -179,7 +186,7 @@ void DalyBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t g
 
 void DalyBmsBle::update() {
   if (this->node_state != espbt::ClientState::ESTABLISHED) {
-    ESP_LOGW(TAG, "[%s] Not connected", this->parent_->address_str());
+    ESP_LOGW(TAG, "[%s] Not connected", ADDR_STR(this->parent_->address_str()));
     return;
   }
 
@@ -676,7 +683,7 @@ bool DalyBmsBle::send_command(uint8_t function, uint16_t address, uint16_t value
                                sizeof(frame), frame, ESP_GATT_WRITE_TYPE_NO_RSP, ESP_GATT_AUTH_REQ_NONE);
 
   if (status) {
-    ESP_LOGW(TAG, "[%s] esp_ble_gattc_write_char failed, status=%d", this->parent_->address_str(), status);
+    ESP_LOGW(TAG, "[%s] esp_ble_gattc_write_char failed, status=%d", ADDR_STR(this->parent_->address_str()), status);
   }
 
   return (status == 0);
