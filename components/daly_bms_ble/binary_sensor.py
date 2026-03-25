70738bd698c9eb9f60013614546366d7f15b6251
Sebastian Muszynski <basti@linkt.de>
2026-03-25 22:08:35 +0100

    Add entity_category to binary sensor and switch schemas

--- components/daly_bms_ble/binary_sensor.py
+++ components/daly_bms_ble/binary_sensor.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import binary_sensor
 import esphome.config_validation as cv
+from esphome.const import ENTITY_CATEGORY_DIAGNOSTIC
 
 from . import CONF_DALY_BMS_BLE_ID, DALY_BMS_BLE_COMPONENT_SCHEMA
 
@@ -21,12 +22,15 @@ BINARY_SENSORS = [
 CONFIG_SCHEMA = DALY_BMS_BLE_COMPONENT_SCHEMA.extend(
     {
         cv.Optional(CONF_BALANCING): binary_sensor.binary_sensor_schema(
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
             icon="mdi:battery-heart-variant"
         ),
         cv.Optional(CONF_CHARGING): binary_sensor.binary_sensor_schema(
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
             icon="mdi:battery-charging"
         ),
         cv.Optional(CONF_DISCHARGING): binary_sensor.binary_sensor_schema(
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
             icon="mdi:power-plug"
         ),
     }
