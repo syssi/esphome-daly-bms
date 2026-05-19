0784510f0dae2e191655cf8a126e4871109b7037
Sebastian Muszynski <basti@linkt.de>
2026-05-19 15:18:28 +0200

    Move BLE MAC address to secrets.yaml (#104)

--- test-esp32.sh
+++ test-esp32.sh
@@ -6,4 +6,4 @@ else
   C="components"
 fi
 
-esphome -s mac_address B0:A7:32:13:57:72 -s external_components_source $C ${1:-run} ${2:-esp32-ble-example-faker.yaml}
+esphome -s external_components_source $C ${1:-run} ${2:-esp32-ble-example-faker.yaml}
